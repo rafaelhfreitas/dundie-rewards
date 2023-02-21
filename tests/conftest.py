1528fa983cdceeb78b017cf1e8b1482dd8182a88
Rafael Freitas <rafael@ezsolucoes.com.br>
2023-02-20 22:54:33 -0300

    Config gitignore

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 MARKER = """\
 unit: Mark unit tests
 integration: Mark integration tests
@@ -10,3 +12,11 @@ low: Low Priority
 def pytest_configure(config):
     for line in MARKER.split("\n"):
         config.addinivalue_line('markers',line)
+
+
+
+@pytest.fixture(autouse=True)
+def go_to_tmpdir(request):  #dependency injection
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield
