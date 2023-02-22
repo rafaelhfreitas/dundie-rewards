7ae2b00595a3ac39ede700feda706b9fa7556d7e
Rafael Freitas <rafael@ezsolucoes.com.br>
2023-02-21 23:51:31 -0300

    Test

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,16 +1,16 @@
+# import click
 import pkg_resources
-# import rich_click as click
-import click
+import rich_click as click
 from rich import print
 
 from dundie import core
 
-# click.rich_click.USE_RICH_MARKUP = True
-# click.rich_click.USE_MARKDOWN = True
-# click.rich_click.SHOW_ARGUMENTS = True
-# click.rich_click.GROUP_ARGUMENTS_OPTIONS = True
-# click.rich_click.SHOW_METAVARS_COLUMN = True
-# click.rich_click.APPEND_METAVARS_HELP = True
+click.rich_click.USE_RICH_MARKUP = True
+click.rich_click.USE_MARKDOWN = True
+click.rich_click.SHOW_ARGUMENTS = True
+click.rich_click.GROUP_ARGUMENTS_OPTIONS = True
+click.rich_click.SHOW_METAVARS_COLUMN = True
+click.rich_click.APPEND_METAVARS_HELP = True
 
 
 @click.group()
