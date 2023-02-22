2476736e8b1dadc4ccedcc6bdf921642437e0065
Rafael Freitas <rafael@ezsolucoes.com.br>
2023-02-22 00:55:05 -0300

    Add rich console and rich table / refactoring tests

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,6 +1,7 @@
 import pkg_resources
 import rich_click as click
-from rich import print
+from rich.console import Console
+from rich.table import Table
 
 from dundie import core
 
@@ -8,7 +9,7 @@ click.rich_click.USE_RICH_MARKUP = True
 click.rich_click.USE_MARKDOWN = True
 click.rich_click.SHOW_ARGUMENTS = True
 click.rich_click.GROUP_ARGUMENTS_OPTIONS = True
-click.rich_click.SHOW_METAVARS_COLUMN = True
+click.rich_click.SHOW_METAVARS_COLUMN = False
 click.rich_click.APPEND_METAVARS_HELP = True
 
 
@@ -24,15 +25,26 @@ def main():
 @main.command()
 @click.argument("filepath", type=click.Path(exists=True))
 def load(filepath):
-    """Loads the file to the database
+    """Loads the file to the database.
+
+    ## Features
 
     - Validates data
     - Parses the file
     - Loads to database
     """
+    table = Table(title="Dunder Mufflin Associates")
+    headers = ["name", "dept", "role", "email"]
+    for header in headers:
+        table.add_column(header, style="green")
+
     result = core.load(filepath)
-    header = ["name", "dept", "role", "email"]
     for person in result:
-        print("-" * 50)
-        for key, value in zip(header, person.split(",")):
-            print(f"[red]{key}[/] -> [magenta]{value.strip()}[/]")
+        table.add_row(*[field.strip() for field in person.split(",")])
+        # table.add_row(*person.split(","))
+        # print("-" * 50)
+        # for key, value in zip(header, person.split(",")):
+        #     print(f"[red]{key}[/] -> [magenta]{value.strip()}[/]")
+
+    console = Console()
+    console.print(table)
--- integration/test_load.py
+++ integration/test_load.py
@@ -25,21 +25,30 @@ Out[20]:
 In [21]:
 
 """
-from subprocess import CalledProcessError, check_output
+# from subprocess import CalledProcessError, check_output
 
 import pytest
+from click.testing import CliRunner
+
+from dundie.cli import load, main
+
+from .constants import PEOPLE_FILE
+
+cmd = CliRunner()
 
 
 @pytest.mark.integration
 @pytest.mark.high
 def test_load_positive_call_load_command():
     """test command load"""
-    out = (
-        check_output(["dundie", "load", "tests/assets/people.csv"])
-        .decode("utf-8")
-        .split("\n")
-    )
-    assert len(out) == 16
+
+    out = cmd.invoke(load, PEOPLE_FILE)
+    # out = (
+    #     check_output(["dundie", "load", PEOPLE_FILE])
+    #     .decode("utf-8")
+    #     .split("\n")
+    # )
+    assert "Dunder Mufflin Associates" in out.output
 
 
 @pytest.mark.integration
@@ -47,9 +56,11 @@ def test_load_positive_call_load_command():
 @pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
 def test_load_negative_call_load_command_with_wrong_params(wrong_command):
     """test command load"""
-    with pytest.raises(CalledProcessError) as error:
-        check_output(
-            ["dundie", wrong_command, "tests/assets/people.csv"]
-        ).decode("utf-8").split("\n")
+    # with pytest.raises(CalledProcessError) as error:
+    #     check_output(
+    #         ["dundie", wrong_command, "tests/assets/people.csv"]
+    #     ).decode("utf-8").split("\n")
 
-    assert "status 2" in str(error.getrepr())
+    out = cmd.invoke(main, wrong_command, PEOPLE_FILE)
+    assert out.exit_code != 0
+    assert f"No such command '{wrong_command}'." in out.output
